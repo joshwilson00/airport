ada0e89cef5463c1b73c4f74211c6cf8e41c2f58
joshwilson00 <joshuawilson1605@gmail.com>
2020-10-07 15:07:27 +0100

    Planes can now take off and land

--- Airport/airport.js
+++ Airport/airport.js
@@ -1,17 +1,26 @@
 class Airport {
+    static airports = [];
     constructor(name){
         this.name = name;
         this.currentPlanes = [];
+        this.constructor.airports.push(this);
     }
 
     land(plane){
+        console.log(`Plane landing from: ${plane.currentLocation}`);
         //Update the planes current location.
         plane.currentLocation = this.name;
         //Add the plane to the array of planes at this airport.
         this.addPlane(plane);
-
+        const rand = (Math.floor(Math.random() * this.constructor.airports.length));
+        plane.destination = this.constructor.airports[rand].name;
     }
-
+    takeOff(plane){
+        console.log(`Plane departing to: ${plane.destination}`);
+        const loc = this.currentPlanes.indexOf(plane);
+        this.currentPlanes.splice(this.currentPlanes.indexOf(plane), 1);
+        // console.log(this.constructor.airports.indexOf(plane.currentLocation));
+    }
     addPlane(plane){
         this.currentPlanes.push(plane);
     }
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const egnm = new Airport('EGNM');
 const egll = new Airport('EGLL');
 const lepa = new Airport('LEPA');
 const plane1 = new Plane(egnm, 200, egll);
-const plane2 = new Plane(egll, 20, egnm);
+const plane2 = new Plane(lepa, 20, egnm);
 const josh = new Person('Josh');
 const sam = new Person('Sam');
 const bag1 = new Bag(20);
@@ -21,8 +21,8 @@ sam.addBag(bag2);
 sam.addBag(bag3)
 plane2.board(sam);
 plane1.board(josh);
+
+
+
 egnm.land(plane1);
-plane1.changeDest(lepa.name);
-egnm.showPlanes();
-lepa.land(plane2);
-lepa.showPlanes();
+egnm.takeOff(plane2);
